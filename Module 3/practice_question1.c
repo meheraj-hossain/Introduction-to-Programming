74815ba1fab869a9525263b1012c76511388fc2d
Meheraj Hossain <mehossain13@gmail.com>
2025-04-13 00:26:06 +0600

    5-0 done

---
+++ Module 3/practice_question1.c
@@ -0,0 +1,175 @@
+
+// In this problem you will be given a floating point number x. You have to print x upto 3 decimal points.
+// #include <stdio.h>
+
+// int main() {
+//     float number0;
+//     scanf("%f", &number0);
+//     printf("%.3f", number0);
+
+//     return 0;
+// }
+
+// // In this problem you will be given two integers a and b, you have to tell if one is the multiple of other. (a is the multiple of b or b is the multiple of a).
+// #include <stdio.h>
+
+// int main()
+// {
+//     int number0, number1;
+//     scanf("%d  %d", &number0, &number1);
+//     // printf("%d  %d", number0, number1);
+
+//     if (number0 > number1)
+//     {
+//         int temp = number1;
+//         number1 = number0;
+//         number0 = temp;
+//     }
+
+//     if (number1 % number0 == 0)
+//         printf("Yes");
+//     else
+//         printf("No");
+
+//     return 0;
+// }
+
+// // In this problem you will be given an integer number N. Print "Zero" if the number is equals to 0 and "Non Zero" Otherwise.
+// #include <stdio.h>
+
+// int main()
+// {
+//     int number0;
+//     scanf("%d", &number0);
+
+//     if (number0 == 0)
+//         printf("Zero");
+//     else
+//         printf("Non Zero");
+
+//     return 0;
+// }
+
+// // In this problem you will be given an integer number N. You will have to add 5 with N and print it
+// #include <stdio.h>
+
+// int main()
+// {
+//     int number0;
+//     scanf("%d", &number0);
+
+//     printf("%d", number0 + 5);
+
+//     return 0;
+// }
+
+// //I know that you're loving practice days! So this task is for you. You need to print "I Love Practice" without the quotation marks. I know that you can do it!
+// #include <stdio.h>
+
+// int main()
+// {
+//     printf("I Love Practice");
+
+//     return 0;
+// }
+
+// // Take two integers A and B as input and output their summation.
+// #include <stdio.h>
+
+// int main()
+// {
+//     int number0, number1;
+//     scanf("%d  %d", &number0, &number1);
+
+//     printf("%d", number0 + number1);
+
+//     return 0;
+// }
+
+// You've learned about variables, right? Now its time to practice them. You need to take an integer A, a very big integer B, a floating value C and a character D as input and output them serially.
+//  #include <stdio.h>
+
+// int main()
+// {
+//     int number0;
+//     long long int number1;
+//     float number2;
+//     char a;
+//     scanf("%d\n%lld\n%f\n%c", &number0, &number1, &number2, &a);
+
+//     printf("%d\n%lld\n%.2f\n%c", number0, number1, number2, a);
+
+//     return 0;
+// }
+
+// I know and you also know that you love practice day so much. So this task is for you. You will be given a positive integer N, you need to print "I Love Practice" N times.
+// Here positive integer means those integers that are greater than 0.
+// #include <stdio.h>
+
+// int main()
+// {
+//    unsigned int number0;
+
+//     scanf("%u", &number0);
+
+//     for (int i = 0; i < number0; i++)
+//     {
+//        printf("I Love Practice \n");
+//     }
+
+//     return 0;
+// }
+
+// You will be given a positive integer N, you need to print from 1 to N and besides the value, print Yes or No Print Yes if the value is divisible by 5 and print No otherwise.
+// #include <stdio.h>
+
+// int main()
+// {
+//     unsigned int number0;
+
+//     scanf("%u", &number0);
+
+//     for (int i = 1; i <= number0; i++)
+//     {
+//         if (number0 % 5 == 0)
+//             printf("%d Yes \n", i);
+//         else
+//             printf("%d No \n", i);
+//     }
+
+//     return 0;
+// }
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    printf("Hello, world! I am learning C programming language. ^_^ \nProgramming is fun and challenging. /\\/\\/\\ \n I want to give my 100%%");
+
+    return 0;
+}
+
+// #include <stdio.h>
+// #include <string.h>
+// #include <math.h>
+// #include <stdlib.h>
+
+// int main()
+// {
+//     long long int taka = 0;
+//     scanf("%lld", &taka);
+
+//     if (taka < 1001)
+//         printf("Bad luck! \n");
+//     else
+//         printf("I will buy Punjabi \n");
+//     taka -= 1000;
+//     if (taka >= 500)
+//         printf("I will buy new shoes \nAlisa will buy new shoes /\\/\\/ \n");
+
+//     return 0;
+// }
