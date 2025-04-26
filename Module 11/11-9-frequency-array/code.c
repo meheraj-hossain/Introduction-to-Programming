2ee0c9ddb0b2626fc00775702c62dfed38de1a5c
Meheraj Hossain <mehossain13@gmail.com>
2025-04-27 01:12:14 +0600

    11-9 done

--- Module 11/11-9-frequency-array/code.c
+++ Module 11/11-9-frequency-array/code.c
@@ -1,26 +1,53 @@
-#include <stdio.h>
-#include <string.h>
+// #include <stdio.h>
+// #include <string.h>
 
-char a[10000001];
+// char a[10000001];
 
-int main()
-{
+// int main()
+// {
     
-    scanf("%s", a);
-    int len = strlen(a);
-    int freq[26] = {0};
-    // printf("%s", a);
+//     scanf("%s", a);
+//     int len = strlen(a);
+//     int freq[26] = {0};
+//     // printf("%s", a);
 
-    for (int i = 0; i < len; i++)
-        freq[a[i] - 'a']++;
+//     for (int i = 0; i < len; i++)
+//         freq[a[i] - 'a']++;
 
-    for (int i = 0; i < 26; i++)
+//     for (int i = 0; i < 26; i++)
+//     {
+//         if (freq[i] > 0)
+//         {
+//             printf("%c : %d\n", i + 'a', freq[i]);
+//         }
+//     }
+
+//     return 0;
+// }
+
+#include <stdio.h>
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int a[n];
+    int freq[m+1];
+
+    for (int i = 0; i < m+1; i++)
     {
-        if (freq[i] > 0)
-        {
-            printf("%c : %d\n", i + 'a', freq[i]);
-        }
+        freq[i] = 0;
+    }
+    
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+        freq[a[i]]++;
     }
 
+    for (int i = 1; i < m+1; i++)
+    {
+       printf("%d\n", freq[i]);
+    }
+    
     return 0;
 }
