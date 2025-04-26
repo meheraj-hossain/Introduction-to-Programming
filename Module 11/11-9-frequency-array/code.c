b03bba0be4ff6445803d33a0e1a8911d9701d80c
Meheraj Hossain <mehossain13@gmail.com>
2025-04-27 01:53:04 +0600

    11-9 done

--- Module 11/11-9-frequency-array/code.c
+++ Module 11/11-9-frequency-array/code.c
@@ -25,29 +25,78 @@
 //     return 0;
 // }
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main() {
-    int n, m;
-    scanf("%d %d", &n, &m);
-    int a[n];
-    int freq[m+1];
+// int main() {
+//     int n, m;
+//     scanf("%d %d", &n, &m);
+//     int a[n];
+//     int freq[m+1];
 
-    for (int i = 0; i < m+1; i++)
-    {
-        freq[i] = 0;
-    }
+//     for (int i = 0; i < m+1; i++)
+//     {
+//         freq[i] = 0;
+//     }
     
-    for (int i = 0; i < n; i++)
+//     for (int i = 0; i < n; i++)
+//     {
+//         scanf("%d", &a[i]);
+//         freq[a[i]]++;
+//     }
+
+//     // for single line we could use while loop
+//     // while (scanf("%d", &a) != EOF)
+//     // {
+//     //     freq[a]++;
+//     // }
+//     // then we would not need a[n] but int a;
+//     for (int i = 1; i < m+1; i++)
+//     {
+//        printf("%d\n", freq[i]);
+//     }
+
+//     return 0;
+// }
+
+#include <stdio.h>
+#include <string.h>
+
+char s1[1000001], s2[10000001];
+int main() {
+    scanf("%s %s", s1, s2);
+    int freq1[26] = {0}; int freq2[26] = {0};
+    int s1Len = strlen(s1);
+    int s2Len = strlen(s2);
+
+    for (int i = 0; i < s1Len; i++)
     {
-        scanf("%d", &a[i]);
-        freq[a[i]]++;
+        freq1[s1[i] - 'a'] ++;
     }
 
-    for (int i = 1; i < m+1; i++)
+    for (int i = 0; i < s2Len; i++)
     {
-       printf("%d\n", freq[i]);
+        freq2[s2[i] - 'a'] ++;
     }
+    int count = 0;
+
+    for (int i = 0; i < 26; i++)
+    {
+        if (freq1[i] > 0 && freq2[i] )
+        {
+            // printf("%d %d\n", freq1[i], freq2[i]);
+            
+            if (freq1[i] <= freq2[i])
+            {
+                count += freq1[i];
+            } else if (freq1[i] > freq2[i]){
+                count += freq2[i];
+            }
+            
+        }
+        
+    }
+    printf("%d", count);
+    
     
     return 0;
 }
